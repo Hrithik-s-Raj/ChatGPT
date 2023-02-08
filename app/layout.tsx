dbd6d14f3024bdb6dcfed8e405d0cb8b49b21d3f
Hrithik.s.raj <84892078+Hrithik-s-Raj@users.noreply.github.com>
2023-02-08 23:21:31 +0530

    basic Setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
-import './globals.css'
+import "./globals.css";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
@@ -12,7 +12,11 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <div>
+          <div className="bg-[#343541] flex-1">{children}</div>
+        </div>
+      </body>
     </html>
-  )
+  );
 }
